90f9a150e3f40cdb5586fe993bb908bc381351d2
V0id1ess <ryanfagan827@gmail.com>
2025-12-10 15:23:58 -0500

    Test values for Field Constants to get ready for auto planning.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/autonomous/FieldConstants.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.utility.autonomous;
+
+import com.acmerobotics.dashboard.config.Config;
+
+import org.firstinspires.ftc.teamcode.utility.dataTypes.Pose;
+
+public class FieldConstants {
+    @Config
+    public static class Start {
+        public static Pose redLong = new Pose(23.8, 63, 0.0);
+        public static Pose redShort = new Pose(44.0, -59.5, 54.0);
+        public static Pose blueLong = new Pose(-23.8, 63, 0.0);
+        public static Pose blueShort = new Pose(-44.0, -59.5, -54.0);
+    }
+    @Config
+    public static class Spike {
+        public static Pose red1 = new Pose(30.5, 36, 90.0);
+        public static Pose red2 = new Pose(30.5, 12.0, 90.0);
+        public static Pose red3 = new Pose(30.5, -12.0, 90.0);
+        public static Pose blue1 = new Pose(-30.5, 36, -90.0);
+        public static Pose blue2 = new Pose(-30.5, 12.0, -90.0);
+        public static Pose blue3 = new Pose(-30.5, -12.0, -90.0);
+
+
+    }
+
+    @Config
+    public static class Launch {
+        public static Pose redLong = new Pose(16.0, 56.0, 15.0);
+        public static Pose redShort = new Pose(12.0, -23.0, 46.0);
+        public static Pose blueLong = new Pose(-16.0, 56.0, -15.0);
+        public static Pose blueShort = new Pose(-12.0, -23.0, -46);
+    }
+}
